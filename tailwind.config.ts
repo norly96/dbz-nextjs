856ca16a61c50fa358ac497295b36938ccf54417
Norluis Galvez <norly021196@gmail.com>
2024-10-23 23:30:32 -0400

    add icon and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        saiyansans: ["var(--font-saiyan-sans)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
