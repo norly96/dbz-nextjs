8f4c9efcb9c685edba337f9ffce09ee1b26ec92c
Norluis Galvez <norly021196@gmail.com>
2024-10-24 02:29:03 -0400

    add pages

---
+++ src/app/personajes/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CharactersPage = () => {
+  return <div className="text-black dark:text-white">CharactersPage</div>;
+};
+
+export default CharactersPage;
---
+++ src/app/planetas/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlanetPage = () => {
+  return <div className="text-black dark:text-white">PlanetPage</div>;
+};
+
+export default PlanetPage;
