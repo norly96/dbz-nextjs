a53c9ed42c06004ce1bbd1dfe06d67c013aa00be
Norluis Galvez <norly021196@gmail.com>
2024-10-23 23:33:53 -0400

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "../components/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const saiyanSans = localFont({
+  src: "./fonts/Saiyan-Sans.ttf",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${saiyanSans.className} antialiased`}>
+        <Navbar />
         {children}
       </body>
     </html>
